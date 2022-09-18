73c9f06757253bfea8e90bffedb35d4a1b2d718e
falcode <sfalcon666@gmail.com>
2022-09-18 19:42:22 +0200

    loading native

---
+++ components/loading-page.tsx
@@ -0,0 +1,27 @@
+
+interface LoadingPageProps {
+    fill: string;
+    width: 40;
+    viewBox: string;
+}
+
+export const LoadingPage = ({ fill = '#fe6680', width = 40, viewBox = '0 0 120 30' }: LoadingPageProps) => (
+    <div className="w-full h-screen flex items-center justify-center">
+        <div className="w-10 h-10">
+            <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" fill={fill} width={width} height={width} viewBox={viewBox}>
+                <circle cx="15" cy="15" r="15">
+                    <animate attributeName="r" from="15" to="15" begin="0s" dur="0.8s" values="15;9;15" calcMode="linear" repeatCount="indefinite"></animate>
+                    <animate attributeName="fill-opacity" from="1" to="1" begin="0s" dur="0.8s" values="1;.5;1" calcMode="linear" repeatCount="indefinite"></animate>
+                </circle>
+                <circle cx="60" cy="15" r="9" fill-opacity="0.3">
+                    <animate attributeName="r" from="9" to="9" begin="0s" dur="0.8s" values="9;15;9" calcMode="linear" repeatCount="indefinite"></animate>
+                    <animate attributeName="fill-opacity" from="0.5" to="0.5" begin="0s" dur="0.8s" values=".5;1;.5" calcMode="linear" repeatCount="indefinite"></animate>
+                </circle>
+                <circle cx="105" cy="15" r="15">
+                    <animate attributeName="r" from="15" to="15" begin="0s" dur="0.8s" values="15;9;15" calcMode="linear" repeatCount="indefinite"></animate>
+                    <animate attributeName="fill-opacity" from="1" to="1" begin="0s" dur="0.8s" values="1;.5;1" calcMode="linear" repeatCount="indefinite"></animate>
+                </circle>
+            </svg>
+        </div>
+    </div>
+)
