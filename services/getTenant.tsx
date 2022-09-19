76ba6d8dc8c95ea537263562d332e601b78b0d5d
falcode <sfalcon666@gmail.com>
2022-09-19 23:57:36 +0200

    test multitenant code

--- services/getTenant.tsx
+++ services/getTenant.tsx
@@ -1,6 +1,10 @@
 export const getTenantCode = (): string => {
-  if (process.env.NODE_ENV !== 'development' && typeof window !== "undefined" && !window.location.href.includes('localhost')) {
-    return window.location.protocol + "//" + window.location.host;
+  if (process.env.NODE_ENV !== 'development' && !window.location.href.includes('localhost')) {
+    if (window.location.href.includes('//')) {
+      return window.location.href.split('//')[1].split('.')[0];
+    } else {
+      return window.location.href.split('.')[0];
+    }
   }
   return 'refy';
 }
