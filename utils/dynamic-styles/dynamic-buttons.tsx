fcde185aa2179dfc8858bad3823a27f59032534e
falcode <sfalcon666@gmail.com>
2022-10-06 16:29:35 +0200

    changes

--- utils/dynamic-styles/dynamic-buttons.tsx
+++ utils/dynamic-styles/dynamic-buttons.tsx
@@ -2,9 +2,10 @@ import {ButtonStyles} from "../../services/models";
 
 export const DynamicButtons = (primaryColor = "FE6680", styles: ButtonStyles) => {
     let bodyStyles = document.body.style;
+    console.log(styles);
     bodyStyles.setProperty('--button-color', primaryColor );
     bodyStyles.setProperty('--button-filled', styles?.filled ? primaryColor : 'transparent');
     bodyStyles.setProperty('--button-floating-filled', styles?.filled ? primaryColor : '#fff');
-    bodyStyles.setProperty('--button-fontColor', styles?.filled ? primaryColor : '#fff');
+    bodyStyles.setProperty('--button-fontColor', styles?.filled ? '#fff' : primaryColor);
     bodyStyles.setProperty('--button-roundedCorners', typeof styles?.roundedCorners == "number" ? `${(styles.roundedCorners*2/100)}rem` : '0.5rem');
 }
