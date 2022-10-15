b89cda11d2230ac2340f9a4f07a9d862e3309fbb
ab <alex@grupodesa.es>
2022-10-15 13:01:14 +0200

    Tontería cards

--- utils/dynamic-styles/dynamic-buttons.tsx
+++ utils/dynamic-styles/dynamic-buttons.tsx
@@ -7,4 +7,5 @@ export const DynamicButtons = (primaryColor = "FE6680", styles: ButtonStyles) =>
   bodyStyles.setProperty('--button-floating-filled', styles?.filled ? primaryColor : '#fff');
   bodyStyles.setProperty('--button-fontColor', styles?.filled ? '#fff' : primaryColor);
   bodyStyles.setProperty('--button-roundedCorners', typeof styles?.roundedCorners == "number" ? `${(styles.roundedCorners * 2 / 100)}rem` : '0rem');
+  bodyStyles.setProperty('--card-roundedCorners', typeof styles?.roundedCorners == "number" ? `${Math.min(1, styles.roundedCorners * 1.5 / 100)}rem` : '0rem');
 }
