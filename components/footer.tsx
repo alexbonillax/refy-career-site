088d3d0358ff7de4f981f36a2835f95fbc0a22d1
falcode <sfalcon666@gmail.com>
2022-10-17 13:50:47 +0200

    link footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "next-i18next";
 import Image from "next/image";
+import Link from "next/link";
 import { logo } from "../assets/svg"
 
 const Footer = () => {
@@ -7,13 +8,14 @@ const Footer = () => {
   return (
     <>
       <footer className="pt-10 py-20 bg-white">
+        <Link href='https://try.refyapp.com'>
         <a className="flex-column flex-align-justify-center cursor-pointer" target="_blank">
           <p className="font-hint mb-1">{t('powered-by')}</p>
-          {/* <app-refy-logo className="mt-1"></app-refy-logo> */}
           <div className="flex flex-align-center w-8">
             <Image src={logo} alt={'powered by Refy'} width={65} height={32} />
           </div>
         </a>
+        </Link>
       </footer>
     </>
   )
