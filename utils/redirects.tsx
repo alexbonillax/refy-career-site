147d688be05d109fd2e52c34483adf7378a0fb17
falcode <sfalcon666@gmail.com>
2022-10-17 17:35:13 +0200

    yeeepi

---
+++ utils/redirects.tsx
@@ -0,0 +1,21 @@
+import Company from "../services/models/company"
+
+export const SSRCheck = (companyInfo: Company, translations: any) => {
+  if (!companyInfo || companyInfo.careers.published) {
+    return {
+      redirect: {
+        destination: '/not-found',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {
+      _nextI18Next: translations._nextI18Next,
+      pageProps: {
+        companyInfo,
+      }
+    }
+  }
+}
