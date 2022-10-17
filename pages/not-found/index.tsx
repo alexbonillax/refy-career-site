ad314b187e71e4b453461d775d07e26fccf7e2ee
falcode <sfalcon666@gmail.com>
2022-10-17 18:05:58 +0200

    yeeepi

--- pages/not-found/index.tsx
+++ pages/not-found/index.tsx
@@ -1,7 +1,7 @@
 export const NotFound = () => (
-  <div className="w-full h-screen flex items-center justify-center">
-    <h1>{`Sorry, this page doesn't exist yet`}</h1>
-    <p>404</p>
+  <div className="w-full h-screen flex flex-col items-center justify-center">
+    <h1 className="font-title">{`Sorry, this page doesn't exist yet`}</h1>
+    <p className="font-big-title">404</p>
   </div>
 )
 
