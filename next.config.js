3f7b29eb59edfe0bd6ef9c1dc213d204827d3117
falcode <sfalcon666@gmail.com>
2022-10-26 18:31:17 +0200

    nextjs upgrade to 13

--- next.config.js
+++ next.config.js
@@ -4,20 +4,20 @@ const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-  // sassOptions: {
-  //   includePaths: [path.join(__dirname, 'styles')],
-  // },
-  // images: {
-  //   formats: ['image/avif', 'image/webp'],
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'refy.s3-eu-west-1.amazonaws.com',
-  //       pathname: '/images/**',
-  //     },
-  //   ],
-  // },
-  // i18n,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'refy.s3-eu-west-1.amazonaws.com',
+        pathname: '/images/**',
+      },
+    ],
+  },
+  i18n,
 }
 
 module.exports = nextConfig
