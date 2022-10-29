aaa51096f35fec752281f845a7cd3e4b43253eb4
falcode <sfalcon666@gmail.com>
2022-10-29 12:16:08 +0200

    caroussel done¿?

--- components/carousel/values-carousel.tsx
+++ components/carousel/values-carousel.tsx
@@ -1,13 +1,17 @@
+import { useTranslation } from "next-i18next";
 import { Value } from "../../services/models/values";
 import { Carousel, CarouselItem } from "./carousel";
 
 
-export const ValuesCarousel = ({ values }: { values: Value[] }) => (
-  <section className="py-10 background-color--grey--0">
-    <h1 className="font-big-title text-center mb-4">Values</h1>
-    <Carousel items={ValuesToCarouselItem(values)} />
-  </section>
-)
+export const ValuesCarousel = ({ values }: { values: Value[] }) => {
+  const {t} = useTranslation("common");
+  return (
+    <section className="py-10 background-color--grey--0">
+      <h1 className="font-big-title text-center mb-4">{t('values')}</h1>
+      <Carousel items={ValuesToCarouselItem(values)} />
+    </section>
+  )
+}
 
 export const ValuesToCarouselItem = (values: Value[]): CarouselItem[] => {
   if (values.length <= 0) { return };
