1327cbf2574b371012395056b9383d2203de7b45
ab <alex@grupodesa.es>
2022-10-30 11:22:19 +0100

    Fix valores

--- components/carousel/values-carousel.tsx
+++ components/carousel/values-carousel.tsx
@@ -1,21 +1,19 @@
-import { useTranslation } from "next-i18next";
-import { Value } from "../../services/models/values";
-import { Carousel, CarouselItem } from "./carousel";
+import {useTranslation} from "next-i18next";
+import {Value} from "../../services/models/values";
+import {Carousel, CarouselItem} from "./carousel";
 
 
-export const ValuesCarousel = ({ values }: { values: Value[] }) => {
-  if (values.length <= 0) { return };
+export const ValuesCarousel = ({values}: { values: Value[] }) => {
   const {t} = useTranslation("common");
   return (
     <section className="py-10 background-color--grey--0">
       <h1 className="font-big-title text-center mb-4">{t('values')}</h1>
-      <Carousel items={ValuesToCarouselItem(values)} />
+      <Carousel items={ValuesToCarouselItem(values)}/>
     </section>
   )
 }
 
 export const ValuesToCarouselItem = (values: Value[]): CarouselItem[] => {
-  if (values.length <= 0) { return [] };
   const items = values.map(value => {
     return {
       name: value.attributes.name,
