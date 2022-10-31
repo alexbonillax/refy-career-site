8f2c1231d32870df1c43db87397f0734493580fa
ab <alex@grupodesa.es>
2022-10-31 11:26:40 +0100

    Pixelperfeccionado values

--- components/carousel/carousel.tsx
+++ components/carousel/carousel.tsx
@@ -7,80 +7,82 @@ export interface CarouselItem {
   name: string;
   picture: string;
   shortDescription: string;
+  description: string;
 }
 
 export const Carousel = ({ items }: { items: CarouselItem[] }) => {
   const [position, setPosition] = useState<number>(0);
   const ref = createRef<Splide>();
   const mainOptions: Options = {
-    type: 'loop',
-    padding: '10rem',
-    gap: '2em',
-    height: 'auto',
+    type: 'slide',
     perPage: 1,
-    fixedWidth: '70rem',
     arrows: false,
     focus: 'center',
+    mediaQuery: 'min',
     breakpoints: {
       1279: {
-        padding: '2rem',
-        fixedWidth: '40rem',
+        fixedWidth: '33.33%',
       },
-      745: {
-        fixedWidth: '25rem',
-      }
+      777: {
+        fixedWidth: '50%',
+      },
+      0: {
+        fixedWidth: '100%',
+      },
     },
   };
-  useEffect(() => { 
-    if (ref.current) { 
+  useEffect(() => {
+    if (ref.current) {
       ref.current.splide.on('moved', (i) => setPosition(i));
-    } 
+    }
   });
   return (
     <>
-      <div className="flex w-full mb-10 justify-evenly mobile:hidden mobile-container--responsive">
-        {
-          items.map((item, i) => (
-            <button key={i} onClick={() => { ref.current.splide.go(i); setPosition(i)}} className={`font-prose ${i === position ? 'font-bold' : ''}`}>{item.name}</button>
-          ))
-        }
-      </div>
-      <Splide options={mainOptions} ref={ref} hasTrack={false} className="my-4">
-        <SplideTrack>
+      <div className="mobile-container--responsive flex-column">
+        <div className="flex justify-evenly mobile:hidden mobile-container--responsive">
           {
             items.map((item, i) => (
-              <SplideSlide key={i} className="box-shadow-container--card br-var">
-                <CarouselCard item={item} />
-              </SplideSlide>
+              <button key={i} onClick={() => { ref.current.splide.go(i); setPosition(i)}} className={`font-subtitle ${i === position ? 'font--dark' : ''}`}>{item.name}</button>
             ))
           }
-        </SplideTrack>
-      </Splide>
-      <div className="flex w-full justify-center">
-      <div className="flex w-52 justify-evenly">
-        {
-          items.map((_, i) => (
-            <div 
-              key={i} 
-              onClick={() => { ref.current.splide.go(i); setPosition(i)}} 
-              className={`w-3 h-3 ${i === position ? 'w-4 background-dynamic' : '' } cursor-pointer rounded-xl box-shadow-container--card background-color--white`}></div>
-          ))
-        }
-      </div>
+        </div>
+        <Splide options={mainOptions} ref={ref} hasTrack={false} className="my-4">
+          <SplideTrack>
+            {
+              items.map((item, i) => (
+                <SplideSlide key={i} className="px-2" onClick={() => { ref.current.splide.go(i); setPosition(i)}}>
+                  <CarouselCard item={item} focused={i === position}/>
+                </SplideSlide>
+              ))
+            }
+          </SplideTrack>
+        </Splide>
+        <div className="flex w-full justify-center">
+          {
+            items.map((_, i) => (
+              <div
+                key={i}
+                onClick={() => { ref.current.splide.go(i); setPosition(i)}}
+                className={`w-2 h-2 mx-1 transition--width ${i === position ? 'w-3 background-dynamic' : '' } cursor-pointer rounded-xl box-shadow-container--card background-color--white`}></div>
+            ))
+          }
+        </div>
       </div>
     </>
   )
 }
 
 
-const CarouselCard = ({ item }: { item: CarouselItem }) => (
-  <div className="flex mobile:flex-col overflow-hidden desktop:flex-row-reverse p-3 br-var background-color--white">
-    <div className="desktop:w-1/2 mobile:w-full desktop:ml-5 mobile:mb-5">
-      <h1 className="font-title">{item.name}</h1>
-      <p className="font-multiline">{item.shortDescription}</p>
-    </div>
+const CarouselCard = ({ item, focused}: { item: CarouselItem, focused: boolean }) => (
+  <div className={`flex-column box-shadow-container--card br-var overflow-hidden cursor-pointer transition--opacity ${focused ? '' : 'opacity--50'}`}>
     <Image
       loader={loaderBucketL} src={item.picture} alt={item.name} width={50} height={50}
-      className="flex desktop:w-1/2 mobile:w-full h-72 desktop:mr-5 mobile:mb-5 relative object-cover br-var" />
+      className="w-full h-60 desktop:mr-5 object-fit--cover" />
+    <div className="flex-column text-center p-3">
+      <h1 className="font-title">{item.name}</h1>
+      <p className="font-subtitle">{item.shortDescription}</p>
+      <div className="font-prose font-prose--condensed text-left mt-1" dangerouslySetInnerHTML={{ __html: item.description }}></div>
+    </div>
+
   </div>
 )
--- components/carousel/values-carousel.tsx
+++ components/carousel/values-carousel.tsx
@@ -6,10 +6,15 @@ import {Carousel, CarouselItem} from "./carousel";
 export const ValuesCarousel = ({values}: { values: Value[] }) => {
   const {t} = useTranslation("common");
   return (
-    <section className="py-10 background-color--grey--0">
-      <h1 className="font-big-title text-center mb-4">{t('values')}</h1>
-      <Carousel items={ValuesToCarouselItem(values)}/>
-    </section>
+    <>
+      {
+        values?.length > 0 &&
+          <section className="py-10 background-color--grey--0">
+              <h1 className="font-big-title text-center mb-4">{t('values')}</h1>
+              <Carousel items={ValuesToCarouselItem(values)}/>
+          </section>
+      }
+    </>
   )
 }
 
@@ -18,7 +23,8 @@ export const ValuesToCarouselItem = (values: Value[]): CarouselItem[] => {
     return {
       name: value.attributes.name,
       picture: value.attributes.pictures[0],
-      shortDescription: value.attributes.shortDescription
+      shortDescription: value.attributes.shortDescription,
+      description: value.attributes.description
     } as CarouselItem
   });
   return items;
