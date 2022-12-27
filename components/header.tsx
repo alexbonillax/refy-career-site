60bac2dd28e4955bf6b4cae096fd7bec210c4ac5
falcode <sfalcon666@gmail.com>
2022-12-27 13:03:41 +0100

    gta

--- components/header.tsx
+++ components/header.tsx
@@ -28,20 +28,15 @@ export const Header = ({ company, title }: HeaderProps) => {
       }
       {
         company.careers.analytics?.google &&
-        <>
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${company.careers.analytics?.google}`}
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', ${company.careers.analytics?.google});
-        `}
-          </Script>
-        </>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${company.careers.analytics?.google}');
+          `}
+        </Script>
       }
       {
         company.attributes.primaryColor &&
