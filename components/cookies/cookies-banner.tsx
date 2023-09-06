cd1e60bfa26b860f3578fc57f5e40dc461146c40
falcode <sfalcon666@gmail.com>
2023-09-06 12:26:14 +0200

    translations settings

--- components/cookies/cookies-banner.tsx
+++ components/cookies/cookies-banner.tsx
@@ -98,7 +98,7 @@ export const CookiesBanner = () => {
                 </div>
               }
               <div className={`flex items-center mobile:justify-center mobile:my-5 h-auto space-x-4 ${editCookies ? 'justify-center' : 'ml-5'}`}>
-                <div className="w-28 font-subtitle !text-sm underline cursor-pointer" onClick={_ => setEditCookies(!editCookies)}>{t('cookies.setting')}</div>
+                <div className="w-28 font-subtitle !text-sm underline cursor-pointer" onClick={_ => setEditCookies(!editCookies)}>{t('cookies.settings')}</div>
                 <ButtonBasic classes="button button--outline-primary" onClick={() => rejectAllCookies()}>{t('cookies.reject')}</ButtonBasic>
                 <ButtonBasic onClick={() => setAllCookies()}>{t('cookies.accept')}</ButtonBasic>
               </div>
